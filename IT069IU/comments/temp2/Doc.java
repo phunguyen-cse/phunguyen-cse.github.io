17feb87533b157d6603ddbcfe277060e0b75a522
phunguyen-cse <phunguyen-cse@users.noreply.github.com>
2019-10-26 09:15:07 +0700

    update index.html

---
+++ IT069IU/comments/temp2/Doc.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+/**
+ * class description
+ */
+
+public class Doc
+{
+	/**
+	 * Doc name
+	 */
+	private String name;
+
+	/**
+	 * Constructor for Doc class
+	 */
+	public Doc()
+	{
+	}
+
+	/** Get doc name
+	 */
+	public String getName()
+	{
+		return name;
+	}
+
+	/** Set doc name
+	 */
+	public void setName(String s)
+	{
+		this.name = s;
+	}
+}
---
+++ IT069IU/comments/temp2/temp/Doc.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+/**
+ * class description
+ * @version 1.0
+ * @author Lin Chen
+ */
+
+public class Doc
+{
+	/**
+	 * Doc name
+	 */
+	private String name;
+
+	/**
+	 * Constructor for Doc class
+	 */
+	public Doc()
+	{
+	}
+
+	/** Get doc name
+	 * @return doc name
+	 */
+	public String getName()
+	{
+		return name;
+	}
+
+	/** Set doc name
+	 * @param doc name
+	 */
+	public void setName(String s)
+	{
+		this.name = s;
+	}
+}
